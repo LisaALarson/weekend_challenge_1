c66d8c966c00d20b63937d35ea7dbd0dda4528b2
LisaALarson <lisalarsonll@gmail.com>
2015-07-19 20:18:43 -0500

    easy mode completed

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
-console.log("javascript is working");
-
 $(document).ready(function(){
-	$("#someForm").submit(function(event){
+
+	// button that removes each employee individually
+	$("body").on('click', "#removeButton", function(){
+		$(this).closest(".eachEmployee").remove();
+	});
+
+	$("#employeeInfoForm").submit(function(event){
 		event.preventDefault();
 
-		var $inputs = $("#someForm :input");
+		var $inputs = $("#employeeInfoForm :input");
 		var employeeObject = {};
 
+		// inputs user information into the employeeObject
 		$inputs.each(function(){
 			employeeObject[this.name] = $(this).val();
-		});
+		});	
 
-		console.log($inputs);
-		console.log(employeeObject);
-		console.log(employeeObject.firstname);
-		console.log(employeeObject.lastname);
-		console.log(employeeObject.employeenumber);
-		console.log(employeeObject.title);
-		console.log(employeeObject.lastreviewscore);
-		console.log(employeeObject.salary);
+		// puts information from the object into the printEmployeeInfo function
+		printEmployeeInfo(employeeObject.firstName, 
+			employeeObject.lastName, 
+			employeeObject.employeeNumber, 
+			employeeObject.employeeTitle, 
+			employeeObject.lastReviewScore, 
+			employeeObject.salary);	
 
-	});
+		// prints employee info onto the html page 
+		// lastReviewScore tags each div for CSS color assignment
+		function printEmployeeInfo(first, last, empNum, title, review, salary){
+			var lastReviewScore="review"+employeeObject.lastReviewScore;
+			$("#prependEmployeesAbove").prepend("<div class='eachEmployee "+lastReviewScore
+				+"'><p>First Name: "
+				+first+"</p><p>Last Name: "
+				+last+"</p><p>Employee Number: "
+				+empNum+"</p><p>Job Title: "
+				+title+"</p><p>Review Score: "
+				+review+"</p><p>Salary: $"
+				+salary+
+				"</p><br><button id='removeButton'>Remove Employee</button><br><hr></div>");
+		}	
 
-	$("body").on('click', '.superbutton', function(){
-		createNewThing();
-	});
+		// clears the text input boxes after submit button is pushed
+		$("#employeeInfoForm")[0].reset();
+	
+	}); // end $("#someForm").submit function
 
-	$("body").on('click', '.awesomeButton', function(){
-		alert("AWESOME BUTTON CLICKED!");
-	});
-});
-
-function createNewThing(){
-	$("body").append("<div></div>");
-	var $el = $("body").children().last();
-	$el.append('THIS SHOULD BE current info');
-	$el.append("<button class='awesomeButton'>THIS SHOULD REMOVE INFO</button>");
-//in this function I need to access the employee information object that was created and display it in the html page.  I also need to have a button to remove the info.
-}
-//several employee infos should be displayed on the screen.  the awesome button should remove that employees info.
+}); // end document ready function
 
